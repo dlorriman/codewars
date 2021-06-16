a4564effb8198fb937245432069ac006436f9b60
Dave Lorriman <dlorriman@pm.me>
2021-06-16 16:47:25 +0200

    Codewars Solution

---
+++ js/solution-0001.js
@@ -0,0 +1,9 @@
+// Define a function named helloWorld.
+// In the function code block, define a variable str, value of str should be a string: "Hello World!"
+// print str to the console.
+
+function helloWorld() {
+  const str = 'Hello World!'
+  console.log(str)
+}
+
