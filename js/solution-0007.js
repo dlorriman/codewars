7c69a29c99c9e9ff0eef3f08c5053cb945edf2b3
Dave Lorriman <dlorriman@pm.me>
2023-06-19 12:19:40 +0200

    codewars challenge solution

---
+++ js/solution-0007.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+
+// function declaration with if else statement
+
+function saleHotdogs(n) {
+  if (n < 5) {
+    return n * 100
+  } else if (n < 10) {
+    return n * 95
+  } else (
+    return n * 90
+  )
+}
+
+// condensed version of above
+
+function saleHotdogs(n) {
+  if (n < 5) return n * 100
+  else if (n < 10) return n * 95
+  else return n * 90
+}
+
+// function declaration with ternary operator
+
+function saleHotdogs(n){
+  return n*(n < 5 ? 100 : n < 10 ? 95 : 90)
+}
+
+// arrow function with ternary operator
+
+const saleHotdogs = n => n*(n < 5 ? 100 : n < 10 ? 95 : 90)
