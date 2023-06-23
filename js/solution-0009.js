f7f28bfa9671ce7b78f746105a15d8a1364e8d2a
Dave Lorriman <dlorriman@pm.me>
2023-06-23 13:22:06 +0200

    codewars challenge solution

---
+++ js/solution-0009.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/57216d4bcdd71175d6000560
+
+function padIt(str, n) {
+  while (n > 0) {
+    if (n % 2 === 0) {
+      str = str + '*'
+    } else {
+      str = '*' + str
+    }
+    n--
+  }
+  return str
+}
+
+padIt('a', 6)
