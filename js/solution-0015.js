2edb4e7d02284297b42f89f38e129dfb94d4d611
dlorriman <dlorriman@pm.me>
2023-07-05 21:18:55 +0200

    codewars challenge solution

--- js/solution-0015.js
+++ js/solution-0015.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/57256064856584bc47000611/train/javascript
 
-function howManySmaller(arr,n){
-    return arr.filter(x => +x.toFixed(2).length
-  }
+function howManySmaller(arr,n) {
+    return arr.filter(x => +x.toFixed(2) < n).length
+}
