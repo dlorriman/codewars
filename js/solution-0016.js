1b2c03060901f8d69d01661904c2d276df164168
dlorriman <dlorriman@pm.me>
2023-07-07 10:05:43 +0200

    codewars challenge solution

---
+++ js/solution-0016.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/57274562c8dcebe77e001012
+
+let strLength = 'abcdefghijklmnopqrstuvwxyz'
+console.log(strLength.length) // output: 26
+
+let strIndex = 'codewars'
+for (let i = 0; i < strIndex.length; i++) console.log(strIndex[i])
+/* output: 
+c
+o
+d
+e
+w
+a
+r
+s
+*/
+
+
