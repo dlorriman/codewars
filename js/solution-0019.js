ee3b95ee3cd50fd6482540510206ca26bef71f59
dlorriman <dlorriman@pm.me>
2023-07-12 12:06:39 +0200

    codewars challenge solution

--- js/solution-0019.js
+++ js/solution-0019.js
@@ -3,3 +3,8 @@
 // stringObject.toUpperCase()
 // stringObject.toLowerCase()
 // stringObject.replace(substring/regexp,replacement)
+
+let str = 'My name is David'
+console.log(str.toUpperCase()) // 'MY NAME IS DAVID'
+console.log(str.toLowerCase()) // 'my name is david'
+
