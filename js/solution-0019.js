49e536b1215c48f3212b4709623ad646bc4e4efb
dlorriman <dlorriman@pm.me>
2023-07-13 10:40:53 +0200

    codewars challenge solution

--- js/solution-0019.js
+++ js/solution-0019.js
@@ -4,7 +4,11 @@
 // stringObject.toLowerCase()
 // stringObject.replace(substring/regexp,replacement)
 
-let str = 'My name is David'
-console.log(str.toUpperCase()) // 'MY NAME IS DAVID'
-console.log(str.toLowerCase()) // 'my name is david'
+let str1 = 'My name is David'
+console.log(str1.toUpperCase()) // 'MY NAME IS DAVID'
+console.log(str1.toLowerCase()) // 'my name is david'
+
+let str2 = 'this is an example.'
+let result = str2[0].toUpperCase()+str2.slice(1)
+console.log(result)
 
