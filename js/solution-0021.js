b4f96d5dc791ac44115bafc9b55410e84eb7694d
dlorriman <dlorriman@pm.me>
2023-07-18 15:02:15 +0200

    codewars challenge solution

--- js/solution-0021.js
+++ js/solution-0021.js
@@ -4,7 +4,7 @@ let humanYearsCatYearsDogYears = function(humanYears) {
     return [humanYears, (15 + ((humanYears > 2) ? (9+(humanYears-2)*4) : humanYears === 2 ? 9 : 0)), (15 + ((humanYears > 2) ? (9+(humanYears-2)*5) : humanYears === 2 ? 9 : 0))]
 }
 
-let humanYearsCatYearsDogYears = function(y) {
+humanYearsCatYearsDogYears = function(y) {
     if (y === 1) return [1, 15, 15]
     else if (y === 2) return [2, 24, 24]
     else return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
