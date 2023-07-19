174e291c6511b4e4c501a589c686d33c80306a5e
dlorriman <dlorriman@pm.me>
2023-07-19 18:02:07 +0200

    codewars challenge solution

--- js/solution-0020.js
+++ js/solution-0020.js
@@ -4,8 +4,16 @@
 // stringObject.charCodeAt(index)
 // String.fromCharCode(num1,num2,numX)
 
-let str = 'abcde'
-for (let i = 0; i < str.length; i++) {
-    console.log(str.charAt(i),str.charCodeAt(i)) 
+function topSecret(str){
+    let shift = start => ch => String.fromCharCode((ch.charCodeAt(0)-start-3+26)%26 + start),
+        match = re => (str.match(re)||[])[1]||'';
+  
+    str = str.replace(/[a-z]/g, shift(97))
+             .replace(/[A-Z]/g, shift(65))
+  
+    answer1 = match(/Top secret files: No\. (\d*)/);
+    answer2 = match(/Super agent (\w*)/);
+    answer3 = match(/stole the (.*) from National Museum/);
+  
+    return str;
 }
-
