9150d6cdaff49add62f8b5cc75cb822ab6493543
dlorriman <dlorriman@pm.me>
2023-07-22 14:28:48 +0200

    codewars challenge solution

---
+++ js/solution-0026.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55eca815d0d20962e1000106
+
+function generateRange(min, max, step){
+    let array = []
+    for(let i = min; i <= max; i += step) {
+        array.push(i)
+    }
+    return array
+}
