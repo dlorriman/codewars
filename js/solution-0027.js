ba3ba08b478a8389d3ffe6f615a87a20bcf175a7
dlorriman <dlorriman@pm.me>
2023-07-25 15:32:02 +0200

    codewars challenge solution

--- js/solution-0027.js
+++ js/solution-0027.js
@@ -1,2 +1,11 @@
 // https://www.codewars.com/kata/574b3b1599d8f897470018f6/
 
+function getRealFloor(n) {
+    return (n > 12) ? n-2 : (n > 0) ? n-1 : n
+}
+
+function getRealFloor(n) {
+    if (n > 12) return n-2 
+    else if (n > 0) return n-1
+    else return n
+}
