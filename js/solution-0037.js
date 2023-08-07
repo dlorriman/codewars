f4df072e2e3b0eefca387ca457a09ccb3fb7d009
dlorriman <dlorriman@pm.me>
2023-08-07 11:00:28 +0200

    codewars challenge solution

--- js/solution-0037.js
+++ js/solution-0037.js
@@ -1,2 +1,11 @@
 // https://www.codewars.com/kata/571d42206414b103dc0006a1
 
+const arr = N => [...Array(n).keys()] : []
+
+const arr = N => {
+    let arr = []
+    for (let i=0; i<N; i++) {
+        arr.push(i)
+    }
+    return arr
+}
