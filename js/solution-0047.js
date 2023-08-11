7c866ea6faf7821b623ad7f47d6859ea374c8560
dlorriman <dlorriman@pm.me>
2023-08-11 11:13:57 +0200

    codewars challenge solution

--- js/solution-0047.js
+++ js/solution-0047.js
@@ -1,5 +1,10 @@
 // https://www.codewars.com/kata/5547929140907378f9000039
 
+function shortcut(string) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    return string.split('').filter(c => !vowels.includes(c)).join('')
+}
+
 function shortcut (string) {
     const vowels = ['a', 'e', 'i', 'o', 'u']
     const result = []
@@ -12,4 +17,17 @@ function shortcut (string) {
     }
 
     return result.join('')
+}
+
+function shortcut(string) {
+    const vowels = 'aeiou'
+    const withoutVowels = string.split('').reduce((acc, c) => {
+        return acc + (vowels.includes(c) ? '' : char)
+    }, '')
+    return withoutVowels
+}
+
+function shortcut(string) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    return string.split('').filter(char => !vowels.has(char)).join('')
 }
