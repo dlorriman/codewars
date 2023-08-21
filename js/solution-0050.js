6ea585b612bb49a62104a3005b6e266cac15ea59
dlorriman <dlorriman@pm.me>
2023-08-21 14:55:44 +0200

    codewars challenge solution

--- js/solution-0050.js
+++ js/solution-0050.js
@@ -1,2 +1,10 @@
 // https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
 
+function warnTheSheep(queue) {
+    let position = queue.length -queue.indexOf('wolf') -1
+    if (position === 0) {
+        return 'Pls go away and stop eating my sheep'
+    } else {
+        return `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`
+    }
+}
