34b7fbb7b392f356fd2f553f45cea17bebcb51dc
dlorriman <dlorriman@pm.me>
2023-08-21 11:06:41 +0200

    codewars challenge solution

--- js/solution-0053.js
+++ js/solution-0053.js
@@ -15,3 +15,11 @@ const multiTable = (number) => {
     }
     return table
 }
+
+function multiTable(number) {
+    let array = []
+    for let (i = 1; i <=10; i++) {
+        array.push(`${i} * ${number} = ${number * i}`)
+    }
+    return array.join('\n')
+}
