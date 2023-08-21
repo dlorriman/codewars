7c82f979897de5931d9e3598d3125caed6a0ba44
dlorriman <dlorriman@pm.me>
2023-08-21 14:55:01 +0200

    codewars challenge solution

--- js/solution-0049.js
+++ js/solution-0049.js
@@ -1,1 +1,8 @@
-// 
+// https://www.codewars.com/kata/56f695399400f5d9ef000af5
+
+function correctTail(body, tail) {
+    let sub = body[body.length-1]
+    if (sub === tail) return true
+  }
+
+const correctTail = (body, tail) => body.endsWith(tail)
