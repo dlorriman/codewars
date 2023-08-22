7edd03ddc18dfca4e93a1f8726b8080a20fcd113
dlorriman <dlorriman@pm.me>
2023-08-22 12:52:51 +0200

    codewars challenge solution

--- js/solution-0055.js
+++ js/solution-0055.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/596c6eb85b0f515834000049
 
-var replaceDots = function(str) {
+let replaceDots = function(str) {
     return str.replace(/\./g, '-');
   }
+
+let replaceDots = s => s.split('.').join('-')
