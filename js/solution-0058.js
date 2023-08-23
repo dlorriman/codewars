cb1350c27319d9920b2d1c827713e0e38c1be58b
dlorriman <dlorriman@pm.me>
2023-08-23 14:16:06 +0200

    codewars challenge solution

---
+++ js/solution-0058.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/59fca81a5712f9fa4700159a
+
+function toBinary(n){
+    let binary = n.toString(2)
+    return +(binary)
+}
+
+function toBinary(n){
+    return +n.toString(2)
+}
