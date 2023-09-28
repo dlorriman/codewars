7bac3c3cbe4f05c37ea51ed0483e12736c969a11
dlorriman <dlorriman@pm.me>
2023-09-28 11:28:23 +0200

    codewars challenge solution

--- js/8kyu/counting_sheep.js
+++ js/8kyu/counting_sheep.js
@@ -39,13 +39,3 @@ function countSheeps(sheep) {
 }
 
 const countSheeps = s => s.filter(Boolean).length
-
-// forEach plus ternary
-function countSheeps(sheep) {
-    let count = 0
-    sheep.forEach(sheep => sheep ? count++ : count)
-    return count
-}
-
-
-
