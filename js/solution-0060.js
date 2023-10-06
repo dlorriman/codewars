079a947129bb13a4242a9378726f11eb2666447d
dlorriman <dlorriman@pm.me>
2023-10-06 14:06:53 +0200

    codewars solution

--- js/solution-0060.js
+++ js/solution-0060.js
@@ -1,13 +1,13 @@
 /* https://www.codewars.com/kata/559590633066759614000063
 
-Given an array of numbers...
-Will it always be an array that's passed in? 1d array?
-Will the array only contain numbers or could it contain other types of data?
-Could there be invalid data?
-Do I need to acccount for unexpected edge cases?
+Input: Given an array of numbers...
+Will it always be an array that's passed in? 1d array? Yes
+Will the array only contain numbers or could it contain other types of data? Only numbers!
+Could there be invalid data? No
+Do I need to acccount for unexpected edge cases? No
 
-Return the highest and lowest numbers in the array.
-As an array?
+Output: Return the highest and lowest numbers in the array.
+As an array? Yes
 
 Examples:
 [1,2,3,4,5] --> [1,5]
