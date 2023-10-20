1afb2afe065721bb0d9b7c3bd831bcb8bf1f2d9b
dlorriman <dlorriman@pm.me>
2023-10-20 14:06:29 +0200

    codewars solution

---
+++ solution-0077.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/51f41b98e8f176e70d0002a8
+
+Given an array of words...
+Write a function sortme, that returns the array in alphabetical order.
+
+Example: sortme(['one', 'two', 'three' ]), ["one", "three", "two"])
+*/
+
+function sortme(arr) {
+    return arr.sort()
+}
