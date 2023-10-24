5826b421698fc0168f92c04e26de5e19dc1398d3
dlorriman <dlorriman@pm.me>
2023-10-24 09:59:36 +0200

    codewars solution

---
+++ solution-0080.js
@@ -0,0 +1,26 @@
+/* https://www.codewars.com/kata/57eaec5608fed543d6000021
+
+Given an array of integers and strings...
+
+Write a function divCon, that takes in an array of integers and strings.
+Return the sum of the integers minus the sum of the string integers.
+
+Examples:
+divCon([9, 3, '7', '3']), 2);
+divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 14); 
+divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 13);
+*/
+
+function divCon(arr) {
+    let intSum = 0
+    let strSum = 0
+
+    for (let item of arr) {
+        if (typeof item === 'number') {
+            intSum += item
+        } else if (!isNaN(item)) {
+            strSum += Number(item)
+        }
+    }
+    return intSum - strSum
+}
