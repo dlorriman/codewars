a240187671b2ff816d6ad9c2efe1ddb9f4da04ac
dlorriman <dlorriman@pm.me>
2023-10-29 14:52:15 +0200

    codewars solution

---
+++ js/solution-0086.js
@@ -0,0 +1,7 @@
+/* https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+Given a string... Write a function stringToArray, that converts the str to an array.
+
+*/
+
+const stringToArray = str => str.split(' ')
