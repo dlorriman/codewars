3e689b84816578466ab60fef76cced2e886daf94
dlorriman <dlorriman@pm.me>
2023-11-07 13:56:50 +0200

    codewars solution

---
+++ js/solution-0095.js
@@ -0,0 +1,14 @@
+/* https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+
+Given an array of 3 strings
+
+Return the array with the order reversed.
+
+Examples:
+fixTheMeerkat(["tail", "body", "head"]), ["head", "body", "tail"]
+
+*/
+
+function fixTheMeerkat(arr) {
+    return arr.reverse()
+}
