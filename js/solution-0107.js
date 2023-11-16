97bedc5ef74dca08ea9bf5497b4a60b61ff6fa96
dlorriman <dlorriman@pm.me>
2023-11-16 12:21:51 +0200

    codewars solution

---
+++ js/solution-0107.js
@@ -0,0 +1,14 @@
+/* https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+Given a string...
+
+Write a function makeUpperCase, that converts the string to uppercase.
+
+Example: 
+"hello" --> "HELLO"
+
+*/
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
