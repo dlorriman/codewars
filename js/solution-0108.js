d617f242760d5b017438e9842e521a8e5f288b15
dlorriman <dlorriman@pm.me>
2023-11-17 15:24:23 +0200

    codewars solution

--- js/solution-0108.js
+++ js/solution-0108.js
@@ -9,3 +9,13 @@ function Ship(draft,crew) {
 Ship.prototype.isWorthIt = function() {
   return this.draft - this.crew * 1.5 > 20
 }
+
+class Ship {
+    constructor(draft, crew) {
+        this.draft = draft
+        this.crew = crew
+    }
+    isWorthIt() {
+        return this.draft - (this.crew * 1.5) > 20
+    }
+}
