0e9b4fa78d6d7ad128c5c9997d0b6313de9f3183
dlorriman <dlorriman@pm.me>
2023-11-17 15:18:58 +0200

    codewars solution

---
+++ js/solution-0108.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/54fe05c4762e2e3047000add */
+
+function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+}
+//YOUR CODE HERE...
+
+Ship.prototype.isWorthIt = function() {
+  return this.draft - this.crew * 1.5 > 20
+}
