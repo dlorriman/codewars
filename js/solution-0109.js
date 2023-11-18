4dae8eb0968f0bf46f2a950c4ed16e7d4dafe362
dlorriman <dlorriman@pm.me>
2023-11-18 15:47:42 +0200

    codewars solution

---
+++ js/solution-0109.js
@@ -0,0 +1,15 @@
+/* https://www.codewars.com/kata/547274e24481cfc469000416 */
+
+class God{
+    /**
+     * @returns Human[]
+     */
+      static create(){
+        // code
+      }
+    }
+    // code
+
+class Human{}
+class Man extends Human{}
+class Woman extends Human{}
