95e33a6edf7886946a5ef10fea9f636bbb703f43
dlorriman <dlorriman@pm.me>
2023-12-04 11:16:48 +0200

    codewars solution

--- solution-0141.js
+++
@@ -1,9 +0,0 @@
-/* https://www.codewars.com/kata/523b623152af8a30c6000027
-
-Given a number, write a function 'square' that returns the number squared.
-
-Example: 3 --> 9
-
-*/
-
-square = n => n**2
