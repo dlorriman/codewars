cf0728ace87b0ef036a3dee5c708b50e04aa01a3
dlorriman <dlorriman@pm.me>
2023-12-05 09:53:32 +0200

    codewars solution

---
+++ js/solution-0146.js
@@ -0,0 +1,8 @@
+/* https://codewars.com/kata/55ca77fa094a2af31f00002a
+*/
+
+let laLigaGoals = 43
+let championsLeagueGoals = 10
+let copaDelReyGoals = 5
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
