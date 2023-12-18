2ef20d7be066bd52558bb7d61acd26f70a389fb6
dlorriman <dlorriman@pm.me>
2023-12-18 14:33:45 +0200

    codewars solution

--- js/solution-0171.js
+++ js/solution-0171.js
@@ -44,3 +44,16 @@ function whatday(num) {
     return days[num-1] || 'Wrong, please enter a number between 1 and 7'
 }
 
+// using an object literal
+function whatday(num) {
+    let days = {
+        1: 'Sunday',
+        2: 'Monday',
+        3: 'Tuesday',
+        4: 'Wednesday',
+        5: 'Thursday',
+        6: 'Friday',
+        7: 'Saturday',
+    }
+    return days[num] || 'Wrong, please enter a number between 1 and 7'
+}
