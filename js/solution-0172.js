8c5688c4c95a7e45f6566b01fcaa942d89e99759
dlorriman <dlorriman@pm.me>
2023-12-19 10:33:54 +0200

    codewars solution

---
+++ js/solution-0172.js
@@ -0,0 +1,10 @@
+/* https://www.codewars.com/kata/5748838ce2fab90b86001b1a/solutions
+
+Given 1/4 of the circumference of a circle...
+Calculate the area of the inscribed square.
+
+*/
+
+function squareArea(A){
+  return (A*2/Math.PI)**2
+}
