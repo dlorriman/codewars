97f8a44a3e1f1102f2b88eae13c1f4890ac5456b
dlorriman <dlorriman@pm.me>
2023-12-30 14:29:35 +0200

    codewars challenge solution

---
+++ js/solution-0183.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023
+
+function validateUsr(username) {
+    const regex = /^[a-z0-9_]{4,16}$/
+    return regex.test(username)
+}
+
+/**
+    - `^`        Start from the beginning of the string.
+    - `[]`       Allow any character specified, including...
+    - `a-z`      anything from a to z,
+    - `0-9`      anything from 0 to 9,
+    - `_`        and underscore.
+    - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    - `$`        End the string right after specified amount of allowed characters is given.
+  */
