e0e5016a941cc0454113570683717c55a9dc66c0
dlorriman <dlorriman@pm.me>
2024-01-09 14:42:42 +0200

    codewars challenge solution

---
+++ js/solution-0199.js
@@ -0,0 +1,12 @@
+/* https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b
+
+Given a number...
+
+If number is <1 return 0
+else 400 / n
+
+*/
+
+function sakuraFall(n) {
+    return n < 1 ? 0 : 400 / n
+}
