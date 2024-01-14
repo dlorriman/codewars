8f4f61b86cee4d8da83fb150da4d0467f6765852
dlorriman <dlorriman@pm.me>
2024-01-14 15:02:58 +0200

    codewars challenge solution

---
+++ js/solution-0204.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/55acfc59c3c23d230f00006d
+
+Given a character (c)....
+
+Write a function getASCII the returns the ASCII value of c.
+
+*/
+
+function getASCII(c) {
+    return c.charCodeAt()
+}
