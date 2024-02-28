188afe947edbf5d6606a7b9dec6ee3d31aee2717
dlorriman <dlorriman@pm.me>
2024-02-28 11:28:23 +0200

    codewars challenge solution

---
+++ js/solution-0258.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/56a29b237e9e997ff2000048
+
+let rooms = {
+	room1: {
+		name: 'First Room',
+		description: 'This is the first room',
+		completed: false
+	},
+	room2: {
+		name: 'Second Room',
+		description: 'This is the second room',
+		completed: false
+	},
+	room3: {
+		name: 'Third Room',
+		description: 'This is the third room',
+		completed: false
+	}
+}
