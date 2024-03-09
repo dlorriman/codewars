465b2ddfc4cf2a5f1eafac0c28e18693b7d1df4a
dlorriman <dlorriman@pm.me>
2024-03-09 09:52:42 +0200

    codewars challenge solution

---
+++ js/solution-0269.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/514aa0dc21607ae236000017
+
+// Rewrite the code so that greetAbe() actually greets Abe.
+
+const greetAbe = () => {
+	const name = 'Abe'
+	return 'Hello, ' + name + '!'
+}
+
+const greetBen = () => {
+	const name = 'Ben'
+	return 'Hello, ' + name + '!'
+}
