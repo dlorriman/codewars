3fab6a2ef7d809a20b43af39755babc96b352656
dlorriman <dlorriman@pm.me>
2024-03-11 11:58:35 +0200

    codewars challenge solution

--- solution-0270.js
+++ solution-0270.js
@@ -19,3 +19,11 @@ function neutralise(s1, s2) {
 	}
 	return newStr
 }
+
+function neutralise(s1, s2) {
+	let res = ''
+	for (let i = 0; i < s1.length; i++) {
+		res += s1[i] !== s2[i] ? '0' : s1[i]
+	}
+	return res
+}
