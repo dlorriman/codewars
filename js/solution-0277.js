ed8abd163018de8701d447269b73285401e17125
dlorriman <dlorriman@pm.me>
2024-03-18 13:34:49 +0200

    codewars challenge solution

---
+++ js/solution-0277.js
@@ -0,0 +1,10 @@
+/* https://www.codewars.com/kata/545993ee52756d98ca0010e1
+
+Given an array and a function as arguments.
+
+Write a function called none, that accepts two arguments, an array and a function.
+Return true if the function returns false for all of the items in the array. */
+
+function none(arr, fun) {
+	return arr.every(item => !fun(item))
+}
